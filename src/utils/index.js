c526915a60bf6218c75120f6fcc8f59a5280366e
HongBeen <redbean-2@naver.com>
2022-07-04 02:45:41 +0900

    add: lazy loading rootMargin

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,22 +3,31 @@ export const getDom = (selector) => {
 };
 
 export const imageLazyLoading = (elements) => {
-	const io = new IntersectionObserver((entries, observer) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				const img = entry.target.querySelector("img");
-				img.src = entry.target.dataset.url;
+	const io = new IntersectionObserver(
+		(entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					const img = entry.target.querySelector("img");
+					img.src = entry.target.dataset.url;
 
-				observer.unobserve(entry.target);
-			}
-		});
-	});
+					observer.unobserve(entry.target);
+				}
+			});
+		},
+		{
+			rootMargin: "100px 0px",
+		}
+	);
 
 	elements.forEach((element) => io.observe(element));
 };
 
 export const getLocalStorage = (key) => {
 	return window.localStorage.getItem(key);
+};
+//Set으로 구성하기
+export const removeSearchHistory = (key) => {
+	window.localStorage.removeItem(key);
 };
 export const setLocalStorage = (key, value) => {
 	console.log(`set localStorage: ${key}`);
