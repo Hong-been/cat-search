9f8c62c8930f97b670b3ddc6029f9cfeb4f8fdd6
HongBeen <redbean-2@naver.com>
2022-07-04 23:26:46 +0900

    refactor: remove an init variable

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,14 +1,15 @@
 import Store from "../core/Store.js";
 
 const loadingStoreInit = {isLoading: false};
-
-const EMPTY_IMAGE = {
-	name: "",
-	url: "",
-	temperament: "",
-	origin: "",
+const modalStoreInit = {
+	isModalShow: false,
+	image: {
+		name: "",
+		url: "",
+		temperament: "",
+		origin: "",
+	},
 };
-const modalStoreInit = {isModalShow: false, image: EMPTY_IMAGE};
 
 export const loadingStore = new Store(loadingStoreInit);
 export const modalStore = new Store(modalStoreInit);
