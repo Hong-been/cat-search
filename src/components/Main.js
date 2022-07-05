9379e640f921e96693db40e45d4e4747f4e646b8
HongBeen <redbean-2@naver.com>
2022-07-05 17:39:16 +0900

    fix: create a child component in constructor

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,7 @@ import SearchResult from "./SearchResult.js";
 export default class Main extends BaseComponent {
 	constructor(target, props) {
 		super(target, props);
-	}
 
-	componentDidMount() {
 		new SearchResult(this.element, {
 			results: this.props.results,
 			onImageClick: this.props.onImageClick,
